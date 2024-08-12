930d51cf14aae323d7f77cac17c5f5e7bdcfa271
francyfox <7info7web@gmail.com>
2024-08-12 16:13:49 +0500

    fix: remove index

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,27 @@
 <script setup lang="ts">
-import { loadFireflyPreset } from '@tsparticles/preset-firefly'
-import { tsParticles } from '@tsparticles/engine'
-import { useMessage } from 'naive-ui'
-import PortSectionIntro from '~/components/port-section/PortSectionIntro.vue'
-import PortModalFeedback from '~/components/port-modal/PortModalFeedback.vue'
+import PortSectionPortfolio from '~/components/port-section/PortSectionPortfolio.vue'
 
 const { locale } = useI18n()
-const message = useMessage()
-const store = useCommonStore()
-const pageStore = usePageStore()
-const { commonData } = storeToRefs(store)
-const { pageData } = storeToRefs(pageStore)
-const hasKadet = ref(false)
-const showModalFeedback = ref(false)
+const store = usePageStore()
+const { pageData } = storeToRefs(store)
+const commonStore = useCommonStore()
+const { commonData } = storeToRefs(commonStore)
+
+const { status } = useAsyncData(() => store.fetchPage('9940073b-e68b-4e88-a417-85cbea4ac034'))
 
 if (commonData.value?.seo) {
   useSeoMeta(commonData.value?.seo)
 }
-const { status } = useAsyncData(() => pageStore.fetchPage('d282977a-f03f-47fc-8b2a-a01d2fd98f40'))
 
-watch(locale, () => pageStore.fetchPage('d282977a-f03f-47fc-8b2a-a01d2fd98f40'))
-
-if (import.meta.client) {
-  await loadFireflyPreset(tsParticles)
-}
-
-onMounted(() => {
-  if (!localStorage.getItem('port-error')) {
-    hasKadet.value = true
-  }
-
-  showModalFeedback.value = window.location.hash === '#feedback'
-
-  window.addEventListener('hashchange', () =>
-    showModalFeedback.value = window.location.hash === '#feedback',
-  )
-})
+watch(locale, () => store.fetchPage('9940073b-e68b-4e88-a417-85cbea4ac034'))
 </script>
 
 <template>
-  <div class="page home-page relative">
-    <port-section-intro
-      v-if="pageData.sections"
-      :data="pageData.sections[0]"
-      :loading="status !== 'success'"
-    />
-    <port-modal-feedback v-model="showModalFeedback" />
-    <div
-      class="flex absolute bottom-[-5rem] left-[5%] z-20 bg-transparent"
-      title="I see you..."
-    >
-      <img
-        src="/img/kedet-war-room.gif"
-        alt="kedet-war-room"
-        width="120"
-        height="120"
-      >
-    </div>
-  </div>
+  <port-section-portfolio
+    v-if="pageData?.sections"
+    :data="{ title: pageData.title, slider: pageData?.sections[0].data }"
+    :loading="status !== 'success'"
+  />
 </template>
 
 <style scoped>
